1b9035018e3391d40002fcada2f4a7027fcf8116
RGlover80 <richard.glover@nostringnetworks.com>
2014-07-25 13:14:33 +0100

    List of new games

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -7,6 +7,7 @@ angular.module('MainCtrl', []).controller('MainController', function($scope, $ht
 	 $http({method: 'GET', url: '/api/images'}).
     success(function(data, status, headers, config) {
       $scope.images = data;
+      console.log(data);
     }).
     error(function(data, status, headers, config) {
       // called asynchronously if an error occurs
